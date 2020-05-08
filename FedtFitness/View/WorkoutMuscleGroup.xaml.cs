4c5260e1220f0613257497978462dc315734d5da
Kimon Kremyzas <kimo0016@edu.easj.dk>
2020-05-08 21:44:48 +0200

    Renamed WorkoutMuscleGroups to WorkoutMuscleGroup

--- FedtFitness/View/WorkoutMuscleGroup.xaml.cs
+++ FedtFitness/View/WorkoutMuscleGroup.xaml.cs
@@ -20,9 +20,9 @@ namespace FedtFitness.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class WorkoutMuscleGroups : Page
+    public sealed partial class WorkoutMuscleGroup : Page
     {
-        public WorkoutMuscleGroups()
+        public WorkoutMuscleGroup()
         {
             this.InitializeComponent();
         }
