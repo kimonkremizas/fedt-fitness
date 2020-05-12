f120986a689e39cc86425af29a30db7c4ab08fa2
Kimon Kremyzas <kimo0016@edu.easj.dk>
2020-05-12 16:17:50 +0200

    Deleted EquipmentVieModel and MuscleGroupViewModel

--- FedtFitness/Model/Musclegroup.cs
+++ FedtFitness/Model/Musclegroup.cs
@@ -11,25 +11,30 @@ namespace FedtFitness.Model
         public int _muscleId;
         public string _name;
 
-        public MuscleGroup()
-        {
+        //public MuscleGroup()
+        //{
             
-        }
+        //}
         public MuscleGroup(int muscleId, string name)
         {
             _muscleId = muscleId;
             _name = name;
         }
 
-        public int MGID
+        public int mgId
         {
             get { return _muscleId;}
             set { _muscleId = value;}
         }
-        public string MGName
+        public string mgName
         {
             get { return _name;}
             set { _name = value;}
+        }
+
+        public override string ToString()
+        {
+            return $"{mgName}";
         }
     }
 }
