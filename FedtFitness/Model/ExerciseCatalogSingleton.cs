989dccd4f211169850088c4c1a8e3cfaca35747a
Kimon Kremyzas <kimo0016@edu.easj.dk>
2020-05-14 01:43:36 +0200

    exercises fake data

--- FedtFitness/Model/ExerciseCatalogSingleton.cs
+++ FedtFitness/Model/ExerciseCatalogSingleton.cs
@@ -15,10 +15,14 @@ namespace FedtFitness.Model
         private ExerciseCatalogSingleton()
         {
            Exercises = new ObservableCollection<Exercise>();
-           Exercises.Add(new Exercise(1,"pushups",15, 1, 1, "Train your chest"));
-           Exercises.Add(new Exercise(2, "lifts", 15, 2, 1, "Train your chest"));
-           Exercises.Add(new Exercise(3, "pushs", 15, 2,1, "Train your chest"));
-
+           Exercises.Add(new Exercise(1, "T Raises", 15, 2, 1, "Train your chest"));
+           Exercises.Add(new Exercise(2, "Single-Arm Dumbbell Rows", 15, 2, 1, "Train your chest"));
+           Exercises.Add(new Exercise(3, "Delt Raise", 15, 2,1, "Train your chest"));
+           Exercises.Add(new Exercise(4, "Plank with Lateral Arm Raise", 15, 1, 1, "Train your chest"));
+           Exercises.Add(new Exercise(13, "Arm Circles", 15, 3, 1, "Train your chest"));
+           Exercises.Add(new Exercise(25, "Side leg raises", 15, 4, 1, "Train your chest"));
+           Exercises.Add(new Exercise(26, "Lying leg curl", 15, 4, 1, "Train your chest"));
+           Exercises.Add(new Exercise(31, "Medicine ball slam ", 15, 5, 1, "Train your chest"));
         }
 
         public static ExerciseCatalogSingleton Instance
